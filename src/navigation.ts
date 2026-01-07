1e9b2141208d43e1c02eb7939c42dce99e91437f
aquarius-wing <lwy8wing@gmail.com>
2026-01-07 19:17:28 +0800

    pref: 优化底部导航

--- src/navigation.ts
+++ src/navigation.ts
@@ -33,7 +33,6 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    &copy; ${new Date().getFullYear()} <a class="text-blue-600 underline dark:text-muted" href="https://aquariuswing.com/">Wing’s Blog</a> · Powered by <a class="text-blue-600 underline dark:text-muted" href="https://astro.build/" target="_blank">Astro</a> & <a class="text-blue-600 underline dark:text-muted" href="https://github.com/onwidget/astrowind" target="_blank">AstroWind</a>
   `,
 };
